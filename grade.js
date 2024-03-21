e5b6d2451539405d6aab6f71bf14ed68cbf1268b
Oluwatobi <etob720@gmail.com>
2024-03-21 03:19:37 +0100

    third commit

--- grade.js
+++ grade.js
@@ -1,18 +1,22 @@
 function getGrade(){
     let yourName = document.getElementById("yourName").value;
     let yourScore = document.getElementById("yourScore").value;
-    // alert("Welcome " + yourName + " your grade is " + yourScore)
 
     if (yourScore > 69) {
-        alert("Welcome " + yourName + " your grade is Distinction")
+        alert("Welcome " + yourName + " your grade is Distinction");
+        document.getElementById("display").innerHTML = "Welcome " + yourName + " your grade is Distinction"
     } else if (yourScore > 59) {
-        alert("Welcome " + yourName + " your grade is Upper Credit")
+        alert("Welcome " + yourName + " your grade is Upper Credit");
+        document.getElementById("display").innerHTML = "Welcome " + yourName + " your grade is Upper Credit"
     } else if (yourScore > 49) {
-        alert("Welcome " + yourName + " your grade is Credit")
+        alert("Welcome " + yourName + " your grade is Credit");
+        document.getElementById("display").innerHTML = "Welcome " + yourName + " your grade is Credit"
     } else if (yourScore > 44) {
-        alert("Welcome " + yourName + " your grade is Pass")
+        alert("Welcome " + yourName + " your grade is Pass");
+        document.getElementById("display").innerHTML = "Welcome " + yourName + " your grade is pass"
     } else {
-        alert("Welcome " + yourName + " your grade is Fail")
+        alert("Welcome " + yourName + " your grade is Fail");
+        document.getElementById("display").innerHTML = "Welcome " + yourName + " your grade is fail"
     }
 
 }
